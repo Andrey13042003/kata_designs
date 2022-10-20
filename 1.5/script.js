b929c2d4cc24b6fccea41f2131633bb0d3f4c83f
Andrey13042003 <92572052+Andrey13042003@users.noreply.github.com>
2022-10-20 14:05:30 +0300

    Update script.js

--- 1.5/script.js
+++ 1.5/script.js
@@ -7,11 +7,10 @@ function mobileSlider() {
     mySwiper = new Swiper('.swiper', {
 
     observer: true,
-    slideClass: 'swiper__slide',
+    slideClass: 'swiper-slide',
     observeParents: true,
     slidesPerView: "auto",
     watchOverflow: true,
-    spaceBetween: 20,
     pagination: {
         el: '.swiper-pagination', 
         clickable: true,
@@ -20,27 +19,11 @@ function mobileSlider() {
     slider.dataset.mobile = 'true';
 }
 
-const btn = document.querySelector('.section__btn');
-const list = document.querySelector('.swiper__list');
-const span = document.querySelector('.span')
-btn.addEventListener('click', function(e) {
-    btn.classList.toggle('arrows');
-    list.classList.toggle('swiper__list--active');
-    if (list.classList.contains('swiper__list--active')) {
-        span.textContent = 'Скрыть';
-    }
-    else {
-        span.textContent = 'Показать все';
-    }
-})
-
-mobileSlider();
-
 window.addEventListener('resize', () => {
-    if(window.screen.width < 768 && slider.dataset.mobile == 'false') {
+    if (window.screen.width < 768 && slider.dataset.mobile == 'false') {
         mobileSlider();
     }
-    if(window.screen.width >= 768) {
+    if (window.screen.width >= 768) {
         slider.dataset.mobile = 'false';
 
         if(slider.classList.contains('swiper-initialized')) {
@@ -48,3 +31,17 @@ window.addEventListener('resize', () => {
         }
     }
 });
+
+const btn = document.querySelector('.section__btn');
+const list = document.querySelector('.swiper-wrapper');
+const span = document.querySelector('.span')
+btn.addEventListener('click', function(e) {
+    btn.classList.toggle('arrows');
+    list.classList.toggle('swiper-wrapper--active');
+    if (list.classList.contains('swiper-wrapper--active')) {
+        span.textContent = 'Скрыть';
+    }
+    else {
+        span.textContent = 'Показать все';
+    }
+})
